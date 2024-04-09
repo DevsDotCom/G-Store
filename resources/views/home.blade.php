2a7d05ef9f803f07b0e3efcc196cd88bfc0658ee
GunDevs <gunfx.peemapat@gmail.com>
2024-04-10 04:54:08 +0700

    migrate create database

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+        
+                    <p><strong>Name : </strong>{{Auth::user()->name}}</p>
+                    <p><strong>Email : </strong>{{Auth::user()->email}}</p>
+                    <a href="#" class="btn btn-success">Home</a>
+                    <a href="#" class="btn btn-primary">Product Management</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
